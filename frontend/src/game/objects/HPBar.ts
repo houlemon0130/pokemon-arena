523555df59997a975819ff22e61fea43af2dcff0
wentuo <wentuo.hjj@alibaba-inc.com>
2026-04-29 17:38:18 +0800

    fix: Phaser import 改为 namespace import，修复 Docker 模块解析报错

--- frontend/src/game/objects/HPBar.ts
+++ frontend/src/game/objects/HPBar.ts
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import * as Phaser from "phaser";
 
 export class HPBar {
   private readonly bar: Phaser.GameObjects.Graphics;
--- frontend/src/game/scenes/BattleScene.ts
+++ frontend/src/game/scenes/BattleScene.ts
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import * as Phaser from "phaser";
 
 import { ElectricEffect } from "../effects/ElectricEffect";
 import { FireEffect } from "../effects/FireEffect";
